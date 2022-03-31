addf90f9a0524865a3a890af519f96e1c0bbdbc1
pro100user <51286168+pro100user15@users.noreply.github.com>
2022-04-01 00:35:54 +0300

    inizialize
    Inizialize backend and frontend

---
+++ computer-shop-backend/src/main/java/com/pro100user/computershopbackend/ComputerShopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pro100user.computershopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerShopBackendApplication.class, args);
+	}
+
+}
---
+++ computer-shop-backend/src/test/java/com/pro100user/computershopbackend/ComputerShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro100user.computershopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
